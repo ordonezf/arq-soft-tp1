1ae0a166efa065357cbc2f7c65187d71d0a9406f
Nicolas Dominguez <nicolas7885@hotmail.com>
2018-09-23 23:38:04 -0300

    Add intensive routes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express()
+const noop = () => {};
 
 app.get('/', (req, res) => {console.log('Ive been hit by root!');
                             res.send('[node]Hello, World!\n')});
@@ -11,5 +12,12 @@ app.get('/wait', (req, res) => {
         res.send('[node]Hello World, 5 seconds later!\n');
     }, 5000);
 });
+
+app.get('/intensive', (req, res) => {console.log('Ive been hit by intensive!');
+    var startTime = Date.now();
+    while ((Date.now() - startTime) < 5000) {
+        noop();
+    }
+    res.send('[node]Intensive processing done!\n')});
 
 app.listen(8111, () => console.log('I am listening on port 8111!'))
--- py/app.py
+++ py/app.py
@@ -15,6 +15,14 @@ def wait():
     app.logger.info('Ive been hit by wait!')
     return "[python]I waited 5 seconds!\n"
 
+@app.route('/intensive')
+def intensive():
+    app.logger.info('Ive been hit by intensive!')
+    end_loop_time = time.time() + 5
+    while time.time() < end_loop_time:
+        pass
+    return "[python]I processed stuff!\n"
+
 if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
