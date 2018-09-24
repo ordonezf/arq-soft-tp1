8da0ccdf275ca7115ae77fe5b4153411a05a95b7
Nicolas Dominguez <nicolas7885@hotmail.com>
2018-09-23 22:29:07 -0300

    Use gunicorn logger

--- py/app.py
+++ py/app.py
@@ -1,15 +1,21 @@
 import time
+import logging
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    print('Ive been hit by root!')
+    app.logger.info('Ive been hit by root!')
     return "[python]Hello, World!\n"
 
 @app.route('/wait')
 def wait():
     time.sleep(5)
-    print('Ive been hit by wait!')
+    app.logger.info('Ive been hit by wait!')
     return "[python]I waited 5 seconds!\n"
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
