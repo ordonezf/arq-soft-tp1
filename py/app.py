e4284e30718e3a8d4ef5832d5186791321e87a70
Nicolas Dominguez <nicolas7885@hotmail.com>
2018-09-25 22:41:28 -0300

    Add static file serving to python

--- py/app.py
+++ py/app.py
@@ -1,8 +1,8 @@
 import time
 import logging
-from flask import Flask
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def root():
@@ -22,6 +22,12 @@ def intensive():
     while time.time() < end_loop_time:
         pass
     return "[python]I processed stuff!\n"
+
+@app.route('/static/<path:path>')
+def serve_file(path):
+    app.logger.info('Returning file:' + path)
+    return send_from_directory(app.static_folder, path)
+
 
 if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
